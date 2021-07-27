0022671645ff65a2facaa0af27e54e946af00b3a
andy8421 <yichang8421@qq.com>
2021-07-28 00:41:17 +0800

    添加 Record 记录

--- src/components/money/Types.vue
+++ src/components/money/Types.vue
@@ -1,10 +1,10 @@
 <template>
     <div>
         <ul class="types">
-            <li :class="type==='-' ? 'selected' : '' "
+            <li :class="value==='-' ? 'selected' : '' "
                 @click="selectType('-')">支出
             </li>
-            <li :class="type==='+'&& 'selected'"
+            <li :class="value==='+'&& 'selected'"
                 @click="selectType('+')">收入
             </li>
         </ul>
@@ -13,19 +13,17 @@
 
 <script lang="ts">
     import Vue from "vue";
-    import {Component, Prop,} from "vue-property-decorator";
+    import {Component, Prop} from "vue-property-decorator";
 
     @Component
     export default class Types extends Vue {
-        @Prop() readonly value: string;
-
-        type = "-";
+        @Prop(String) readonly value!: string;
 
         selectType(type: string) {
             if (type !== "-" && type !== "+") {
                 throw new Error("type is unknow.");
             }
-            this.type = type;
+            this.$emit("update:value",type);
         }
 
     }
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,27 +1,41 @@
 <template>
     <Layout class-prefix="layout">
         <NumberPad/>
-        <Tags :data-source.sync="tags"/>
+        <Tags :data-source="tags"/>
         <Notes/>
-        <Types/>
+        <Types :value.sync="record.type"/>
     </Layout>
 </template>
 
-<script>
+<script lang="ts">
     import NumberPad from "@/components/money/NumberPad.vue";
     import Tags from "@/components/money/Tags.vue";
     import Notes from "@/components/money/Notes.vue";
     import Types from "@/components/money/Types.vue";
+    import Vue from "vue";
+    import {Component} from "vue-property-decorator";
 
-    export default {
-        name: "Money",
-        components: {Types, Notes, Tags, NumberPad},
-        data() {
-            return {
-                tags: ['衣', '食', '住', '行']
-            }
+    type Record = {
+        type: string,
+        notes: string,
+        tags: string[],
+        amount: number,
+        createdAt?: Date
+    }
+
+    @Component({
+        components: {Types, Notes, Tags, NumberPad}
+    })
+
+    export default class Money extends Vue {
+        tags = ["衣", "食", "住", "行"];
+        record:Record = {
+            type:'-',
+            notes:'',
+            tags:[],
+            amount:0
         }
-    };
+    }
 </script>
 
 <style lang="scss">
