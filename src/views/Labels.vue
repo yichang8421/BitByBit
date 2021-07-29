192606ba5d0cf23eb18840de98182da4189de238
andy8421 <yichang8421@qq.com>
2021-07-29 23:16:15 +0800

    添加 Labels 样式

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,15 +1,78 @@
 <template>
-    <div>
-        <Layout>
-            <p>Labels.vue</p>
-        </Layout>
-    </div>
+    <Layout>
+        <ol class="tags">
+            <li>
+                <span>衣</span>
+                <Icon name="right"/>
+            </li>
+            <li>
+                <span>食</span>
+                <Icon name="right"/>
+            </li>
+            <li>
+                <span>住</span>
+                <Icon name="right"/>
+            </li>
+            <li>
+                <span>行</span>
+                <Icon name="right"/>
+            </li>
+        </ol>
+        <div class="createTag-wrapper">
+            <Button class="createTag">
+                新建标签
+            </Button>
+        </div>
+    </Layout>
 </template>
 
 <script lang="ts">
-    // import Nav from "@/components/Nav.vue";
-    export default {
-        name: "Labels",
-        // components: {Nav}
+    import Vue from "vue";
+    import {Component} from "vue-property-decorator";
+    import Button from "@/components/Button.vue";
+
+    @Component({
+        components: {Button}
+    })
+    export default class Labels extends Vue {
+
     }
 </script>
+
+<style lang="scss" scoped>
+    .tags {
+        background: white;
+        font-size: 16px;
+        padding-left: 16px;
+
+        > li {
+            min-height: 44px;
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+            border-bottom: 1px solid #e3e3e3;
+
+            svg {
+                width: 16px;
+                height: 16px;
+                color: #666;
+                margin-right: 16px;
+            }
+        }
+    }
+
+    .createTag {
+        background: #767676;
+        color: white;
+        border-radius: 4px;
+        border: none;
+        height: 40px;
+        padding: 0 16px;
+
+        &-wrapper {
+            text-align: center;
+            padding: 16px;
+            margin-top: 128-16px;
+        }
+    }
+</style>
