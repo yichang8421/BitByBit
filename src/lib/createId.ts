802b409e8742a8b090bf2b62c7993ad30edc2c0a
andy8421 <yichang8421@qq.com>
2021-07-31 18:35:57 +0800

    解决一处bug：将 id 存入 localStorage

--- src/lib/createId.ts
+++ src/lib/createId.ts
@@ -1,9 +1,10 @@
-let id:number = parseInt(window.localStorage.getItem("_idMax") || '0',10) || 0;
+let id: number = parseInt(window.localStorage.getItem("_idMax") || "0", 10) || 0;
 
 function createId() {
     id++;
+    window.localStorage.setItem("_idMax", id.toString());
     return id;
 }
 
 // tslint:disable-next-line:no-default-export
-export default createId
+export default createId;
