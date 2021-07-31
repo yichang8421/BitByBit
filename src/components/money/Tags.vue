2bd31b7905f54e2392a31e57ad673a5d0c797111
andy8421 <yichang8421@qq.com>
2021-07-31 14:56:41 +0800

    添加备注

--- src/components/money/Tags.vue
+++ src/components/money/Tags.vue
@@ -37,7 +37,7 @@
         create() {
             const name = window.prompt("请输入标签名");
             if (!name) {
-                window.alert("标签名不能为空");
+                //window.alert("标签名不能为空");
             } else if (this.dataSource) {
                 this.$emit("update:dataSource", [...this.dataSource, name]);
             }
@@ -50,6 +50,7 @@
     .tags {
         font-size: 14px;
         padding: 16px;
+        background: white;
 
         > .current {
             display: flex;
--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import Nav from "@/components/Nav.vue";
 import Layout from "@/components/Layout.vue";
 import Icon from "@/components/Icon.vue";
 Vue.config.productionTip = false;
-// 全局引入Nav标签
+// 引入全局标签
 Vue.component("Layout", Layout);
 Vue.component("Nav", Nav);
 Vue.component("Icon", Icon);
