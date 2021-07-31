c5808ab2687497ffa10544f8b0191be9a227aa86
andy8421 <yichang8421@qq.com>
2021-08-01 01:20:45 +0800

    修复无法渲染 bug。原因是 JSON.parse 将 null 转为空字符输出

--- src/store/recordStore.ts
+++ src/store/recordStore.ts
@@ -8,6 +8,8 @@ const recordStore = {
     recordList: [] as RecordItem[],
 
     fetchRecords() {
+        // JSON.parse的目的是将null转为空字符输出。如果没有使用JSON.parse，那输出的值为null(4个字符)。
+        // 运行代码前最好先清空localStorage。避免出现无法渲染情况
         this.recordList = JSON.parse(window.localStorage.getItem(localStorageKeyName) || "[]") as RecordItem[];
         return this.recordList;
     },
