80622721016319542a329eda2d7395e432552499
andy8421 <yichang8421@qq.com>
2021-07-31 18:27:15 +0800

    添加 tag 删除失败警告

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -51,11 +51,16 @@
 
         removeTag() {
             if (this.tag) {
-                tagListModel.remove(this.tag.id);
+                if (tagListModel.remove(this.tag.id)) {
+                    // this.$router.back();
+                } else {
+                    window.alert("删除失败");
+                }
+
             }
         }
 
-        goBack(){
+        goBack() {
             this.$router.back();
         }
     }
