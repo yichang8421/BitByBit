21c306b4ccc2e662436134ce2956871f9fcbb6ad
andy8421 <yichang8421@qq.com>
2021-07-31 12:50:56 +0800

    添加重定向功能，点击标签跳转至编辑页面

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,11 +1,14 @@
 <template>
     <Layout>
-        <ol class="tags">
-            <li v-for="tag in tags" :key="tag.id">
+        <div class="tags">
+            <router-link class="tag"
+                         v-for="tag in tags"
+                         :key="tag.id"
+                         :to="`/labels/edit/${tag.id}`">
                 <span>{{tag.name}}</span>
                 <Icon name="right"/>
-            </li>
-        </ol>
+            </router-link>
+        </div>
         <div class="createTag-wrapper">
             <button class="createTag"
                     @click="createTag">
@@ -47,7 +50,7 @@
         font-size: 16px;
         padding-left: 16px;
 
-        > li {
+        > .tag {
             min-height: 44px;
             display: flex;
             align-items: center;
